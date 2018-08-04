8ff563f8e1d233e60417ad6f35b5d4bc674b98e5
Cruz Sanchez <cruzsanchez9209@gmail.com>
2018-08-03 19:52:44 -0500

    Initial commit

---
+++ PaperRockScissorsGame/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PaperRockScissorsGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			var PlayerScore = 0;
+			var CPUScore = 0;
+			var GameRound = 0;
+			
+
+			Random rnd = new Random();
+			int CPUChoice = rnd.Next(4);
+
+			Console.WriteLine("Lets play Paper, rock, scissors!");
+
+
+			for (int i = 0; i < 10; i++)
+			{
+				Console.WriteLine(CPUChoice);
+
+
+				Console.ReadLine();
+
+
+
+				Console.WriteLine(CPUChoice);
+			}
+
+
+
+			
+			Console.ReadLine();
+
+
+
+
+
+		}
+    }
+}
